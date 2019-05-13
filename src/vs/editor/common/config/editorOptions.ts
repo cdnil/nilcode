cf0666ef3cad24efc74e610fe0691acbd2bf28c5
Nil Lin <nil@lin.am>
2019-05-13 16:23:12 +0800

    default editor font family and font size, close #8

--- src/vs/editor/common/config/editorOptions.ts
+++ src/vs/editor/common/config/editorOptions.ts
@@ -2567,7 +2567,7 @@ export class EditorLayoutProvider {
 }
 
 const DEFAULT_WINDOWS_FONT_FAMILY = 'Consolas, \'Courier New\', monospace';
-const DEFAULT_MAC_FONT_FAMILY = 'Menlo, Monaco, \'Courier New\', monospace';
+const DEFAULT_MAC_FONT_FAMILY = '\'Operator Mono\', Menlo, Monaco, \'Courier New\', monospace';
 const DEFAULT_LINUX_FONT_FAMILY = '\'Droid Sans Mono\', \'monospace\', monospace, \'Droid Sans Fallback\'';
 
 /**
@@ -2579,7 +2579,7 @@ export const EDITOR_FONT_DEFAULTS = {
 	),
 	fontWeight: 'normal',
 	fontSize: (
-		platform.isMacintosh ? 12 : 14
+		platform.isMacintosh ? 14 : 14
 	),
 	lineHeight: 0,
 	letterSpacing: 0,
